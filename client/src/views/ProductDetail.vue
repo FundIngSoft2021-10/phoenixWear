775c28c06eed866a19f0991794734029785dd3e4
Camilo19g <candres-garcia@javeriana.edu.co>
2021-03-24 18:52:57 -0500

    initial commit

--- client/src/views/ProductDetail.vue
+++ client/src/views/ProductDetail.vue
@@ -1,8 +1,8 @@
 <template>
-<div>
-   <Header/>
-</div>
- 
+  <v-app>
+    <Header/>
+    
+  </v-app>  
 </template>
 
 <script>
@@ -15,6 +15,4 @@ export default {
 };
 </script>
 
-<style>
-
-</style>
+<style></style>
