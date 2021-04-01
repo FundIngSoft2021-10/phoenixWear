d9a88cd785574a6b19915babae8d4dc37d22e9be
Camilo19g <candres-garcia@javeriana.edu.co>
2021-04-01 17:37:17 -0500

    second commit

---
+++ client/src/components/details/Breadcrumb.vue
@@ -0,0 +1,67 @@
+<template>
+<body>
+    <ul class="breadcrumbs">
+
+        <li class="breadcrumbs_item">
+            
+            <router-link class="breadcrumbs_link" to="/">INICIO</router-link>
+        </li>
+        <li class="breadcrumbs_item">
+            <router-link class="breadcrumbs_link" to="/ropa">ROPA</router-link>
+        </li>
+        <li class="breadcrumbs_item">
+            <a href="" class="breadcrumbs_link breadcrumbs_link--active">ARTICULO</a>
+        </li>
+    </ul>    
+</body>    
+
+
+
+  
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped lang="scss">
+     .breadcrumbs {
+        margin: 0 11.6% 0 11.6%;
+        position: absolute;
+        top: 175px;
+        padding: 0;
+        font-size: 12px;
+        font-family: $montserratRegular-font;
+         
+
+         }
+    .breadcrumbs_item{
+        display: inline-block;
+        padding: 10px;
+        
+    }
+
+    .breadcrumbs_item:not(:last-of-type)::after{
+        content: '\203a';
+        font-size: 22px;
+        padding: 10px;
+        margin: 0 5px;
+        color: #FF8585;
+    }
+
+    .breadcrumbs_link {
+        text-decoration: none;
+        color: #1B1A1A;
+    }
+
+
+    .breadcrumbs_link:hover {
+        color:#FF8585
+    }
+
+    .breadcrumbs_link--active {
+        color:#FF8585;
+    }
+</style>
---
+++ client/src/components/details/OrderStatus.vue
@@ -0,0 +1,52 @@
+<template>
+<body>
+    <ul class="list">
+
+        <li class="list_item">COMPRADO</li>
+        <li class="list_item">ENVIADO</li>
+        <li class="list_item">EN CAMINO</li>
+        <li class="list_item">ENTREGADO</li>
+    </ul>    
+</body>    
+
+
+
+  
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped lang="scss">
+     .list {
+       
+        position: absolute;
+        left: 602px;
+        top: 560px;
+        padding: 0;
+        font-size: 12px;
+        font-family: $montserratRegular-font;
+        text-align: left;
+
+         }
+    .list_item{
+        display: inline-block;
+        padding: 10px;
+        color: #9B9797;
+        text-decoration: none;
+ 
+    }
+    .list_item:not(:last-of-type)::after{
+        content: '\2192';
+        font-size: 22px;
+        padding: 5px;
+        margin: 0 5px;
+        color: #9B9797;
+    }
+
+
+
+</style>
