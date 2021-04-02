ceb5a691db1f9937200d358c5916a4af4ffd74f2
Juan Carlos Suarez <suarezmjuan@gmail.com>
2021-04-02 11:39:23 -0500

    Mejoras visuales

--- client/src/views/Contact.vue
+++ client/src/views/Contact.vue
@@ -2,19 +2,20 @@
   <div>
       <Header/>
       <main>
-          <section>
-            <form class="col">
-                <label for="nombre">NOMBRE</label><br/>
+          <section class="col1" id="formul">
+            <form>
+                <label for="nombre" class="rotulo">NOMBRE</label><br/>
                 <input type="text" id="nombre" name="nombre" class="entrada" required><br>
-                <label for="apellido">APELLIDO</label><br/>
+                <label for="apellido" class="rotulo">APELLIDO</label><br/>
                 <input type="text" id="apellido" name="apellido" class="entrada" required><br>
-                <label for="correo">E-MAIL</label><br/>
+                <label for="correo" class="rotulo">E-MAIL</label><br/>
                 <input type="email" id="correo" name="correo" class="entrada"><br/>
-                <label for="comentarios">MENSAJE </label><br/>
+                <label for="comentarios" class="rotulo">MENSAJE </label><br/>
                 <input type="textarea" id="comentarios" name="comentarios" class="entrada" required><br/>
+                <input type="submit" name="send" class="send" value="ENVIAR COMENTARIOS">
             </form>
           </section>
-          <section class="col">
+          <section class="col2">
               <p>
                 <b>¿Tienes alguna pregunta? Revisa nuestra sección de </b>
                 <router-link class="link" to="/preguntas">preguntas frecuentes.</router-link>
@@ -50,14 +51,22 @@ export default {
   *{
     box-sizing: border-box;
     font-family: $montserratRegular-font;
+    text-align: left;
   }
- .col{
+ .col1{
    float: left;
+   padding-left: 11%;
+   width: 50%;
+ }
+ .col2{
+   float: left;
+   padding-right: 11%;
    width: 50%;
  }
  input.entrada{
    border: 1px black solid;
    width: 95%;
+   background-color: #F5F5F5;
  }
  #comentarios{
    border: 1px black solid;
@@ -65,7 +74,19 @@ export default {
    height: 100px;
  }
  p {
-   padding-top: 45px;
+   padding-top: 30px;
    text-align: center;
  }
+ form{
+   text-align: left;
+ }
+ input.send {
+   color: white;
+   background-color: #ff8585;
+   margin-top: 20px;
+   text-align: center;
+   border-radius: 15px; 
+   padding: 6px;
+ }
+
 </style>
