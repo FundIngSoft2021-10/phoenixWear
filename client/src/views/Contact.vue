8f60191aa9ff3d9eb12501d4dd305c163f860861
Juan Carlos Suarez <suarezmjuan@gmail.com>
2021-04-02 10:48:34 -0500

    Diseño contact us

--- client/src/views/Contact.vue
+++ client/src/views/Contact.vue
@@ -2,16 +2,30 @@
   <div>
       <Header/>
       <main>
-          <h2>Atención al Cliente</h2>
-          <form>
-              <label for="nombre">Nombre:</label>
-              <input type="text" id="nombre" name="nombre" placeholder="Nombres Apellidos" required><br>
-              <label for="correo">Correo donde podamos contactarte: </label>
-              <input type="email" id="correo" name="correo">
-              <br/>
-              <label for="comentarios">Cuentanos como podemos ayudarte: </label>
-              <input type="textarea" id="comentarios" name="comentarios">
-          </form>
+          <section>
+            <form class="col">
+                <label for="nombre">NOMBRE</label><br/>
+                <input type="text" id="nombre" name="nombre" class="entrada" required><br>
+                <label for="apellido">APELLIDO</label><br/>
+                <input type="text" id="apellido" name="apellido" class="entrada" required><br>
+                <label for="correo">E-MAIL</label><br/>
+                <input type="email" id="correo" name="correo" class="entrada"><br/>
+                <label for="comentarios">MENSAJE </label><br/>
+                <input type="textarea" id="comentarios" name="comentarios" class="entrada" required><br/>
+            </form>
+          </section>
+          <section class="col">
+              <p>
+                <b>¿Tienes alguna pregunta? Revisa nuestra sección de </b>
+                <router-link class="link" to="/preguntas">preguntas frecuentes.</router-link>
+              </p>
+              <p>
+                Tambien puedes escribirnos a: pwsupport@phoenixwear.com
+              </p>
+              <p>
+                Contáctanos a nuestro WhatsApp Business +(57)574125
+              </p>
+          </section>
       </main>
   </div>
 </template>
@@ -33,4 +47,25 @@ export default {
 
 </script>
 <style scoped lang="scss">
+  *{
+    box-sizing: border-box;
+    font-family: $montserratRegular-font;
+  }
+ .col{
+   float: left;
+   width: 50%;
+ }
+ input.entrada{
+   border: 1px black solid;
+   width: 95%;
+ }
+ #comentarios{
+   border: 1px black solid;
+   width: 95%;
+   height: 100px;
+ }
+ p {
+   padding-top: 45px;
+   text-align: center;
+ }
 </style>
