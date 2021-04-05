3fb788fbe8d6b1b9c4a9e3f9f08adde1577b3117
monotera <nelson.a.mosquera@gmail.com>
2021-04-05 16:29:07 -0500

    Deletes CORS test

--- client/src/views/Trending.vue
+++ client/src/views/Trending.vue
@@ -1,27 +1,16 @@
 <template>
   <v-app>
     <Header />
-    <h1>{{ products }}</h1>
     <!-- <img src="../assets/imgs/logo.png" alt="Phoenix-logo" height="200" width="200" /> -->
   </v-app>
 </template>
 
 <script>
 import Header from "../components/general/Header.vue";
-import axios from "axios";
+
 export default {
   components: {
     Header,
-  },
-  data() {
-    return {
-      products: null,
-    };
-  },
-  mounted() {
-    axios
-      .get("https://n4mbc432.herokuapp.com/products")
-      .then((response) => (this.products = response.data.product));
   },
 };
 </script>
