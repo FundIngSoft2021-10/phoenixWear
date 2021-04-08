9f48bc1c652b54f22c48c65e958b173150886eb9
monotera <nelson.a.mosquera@gmail.com>
2021-04-08 01:53:52 -0500

    Adds Q&A view

---
+++ client/src/components/Q&A/Info.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-row justify="center">
+    <v-col cols="6">
+      <h1>PREGUNTAS FRECUENTES</h1>
+      <p>
+        ¿Tienes una pregunta? Estamos para ayudarte.<br />
+        si no encuentras la respuesta<br />
+        contacta nuestra Pagina de Contacto.<br />
+        <router-link class="link" to="/contacto"
+          >Pagina de Contacto.</router-link
+        >
+      </p>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+h1,
+p {
+  text-align: center;
+  font-family: $montserratRegular-font;
+}
+h1 {
+  font-family: $montserratSemiBold-font;
+}
+.link {
+  color: $phoenix-color;
+}
+</style>
--- client/src/views/Questions.vue
+++ client/src/views/Questions.vue
@@ -1,6 +1,11 @@
 <template>
   <v-main>
     <Header />
+    <Info class="width inf" />
+    <div class="questions">
+      <Question v-for="i in 6" :key="i" :title="titles[i - 1]" :ans="ans" />
+    </div>
+
     <Footer />
   </v-main>
 </template>
@@ -8,12 +13,39 @@
 <script>
 import Header from "../components/general/Header.vue";
 import Footer from "../components/general/Footer";
+import Info from "../components/Q&A/Info";
+import Question from "../components/Q&A/Question";
 export default {
   components: {
     Header,
     Footer,
+    Info,
+    Question,
+  },
+  data() {
+    return {
+      titles: [
+        "¿Puedo devolver un objeto?",
+        "¿Comó puedo administrar mi cuenta?",
+        "¿Comó puedo vender?",
+        "¿Qué metodos de pago puedo usar?",
+        "¿Comó puedo crear una cuenta?",
+        "¿Cuál es la cobertura de los envíos?",
+      ],
+      ans:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    };
   },
 };
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+.width {
+  width: 70vw;
+  margin: auto;
+  padding-top: 5rem;
+}
+.questions {
+  padding-top: 2rem;
+}
+</style>
