e81a35f86e2d1344a072b99d21653621465027a8
monotera <nelson.a.mosquera@gmail.com>
2021-04-18 17:49:43 -0500

    FailsSafe

--- client/src/components/miCarrito/MiCarrito.vue
+++ client/src/components/miCarrito/MiCarrito.vue
@@ -1,160 +1,142 @@
 <template>
-    <v-main class="main">  
+  <v-main class="main">
+    <v-card class="resumen">
+      <div class="titulo">
+        <p class="remi">Resumen de mi carrito</p>
+        <div class="descrip">
+          <p>Artículo</p>
+          <p>Precio</p>
+        </div>
+        <v-divider class="divider"></v-divider>
+      </div>
 
-        <v-card class="resumen">
-              <div class="titulo">
-               <p class="remi">Resumen de mi carrito</p>
-               <div class="descrip">
-                <p>Artículo</p>
-                <p>Precio</p>
-               </div>
-              <v-divider class="divider"></v-divider>
-              </div>
+      <div class="contenido">
+        <!--Aquí el for para el script-->
+        <div class="prenda">Camisa azul</div>
+        <div class="precio">$140,000</div>
+      </div>
 
-              <div class="contenido"> <!--Aquí el for para el script-->
-                  <div class="prenda">Camisa azul</div>
-                  <div class="precio">$140,000</div>
-              </div>
+      <div class="fin">
+        <div class="txtTotal">TOTAL</div>
+        <div class="numTotal">$140,000</div>
+        <!--Igual depende del script la suma-->
+      </div>
 
-              <div class="fin">
-                 <div class="txtTotal">TOTAL</div>
-                 <div class="numTotal">$140,000</div> <!--Igual depende del script la suma-->
-              </div>
+      <v-btn class="boton" rounded color="#ff8585" dark>
+        Continuar con envío
+      </v-btn>
+    </v-card>
 
-              <v-btn class="boton"
-               rounded
-               color="#ff8585"
-               dark
-              >
-              Continuar con envío
-              </v-btn>
-        </v-card>
-
-    <v-card
-      elevation="0"
-      max-width="710"
-      class="productos"
-    >
-      <v-virtual-scroll
-        :items="items"
-        height="470"
-        item-height="200"
-      >
+    <v-card elevation="0" max-width="710" class="productos">
+      <v-virtual-scroll :items="items" height="470" item-height="200">
+        <template v-slot:default="{ item }">
           <v-list-item class="item">
+            <img src="../../assets/imgs/logo.png" height="100" width="100" />
 
-          <img src="../../assets/imgs/logo.png" height="100" width="100"/>
-  
-          <div class="prendaInfo">
-              <p>Camisa azul</p>
+            <div class="prendaInfo">
+              <p>{{ item.name }}</p>
               <p>$140,000 IVA incluido</p>
-          </div>
+            </div>
 
-          <div class="icons">
-           <i class="far fa-heart"></i>
-           <i class="fas fa-trash"></i>
-          </div>
-
+            <div class="icons">
+              <i class="far fa-heart"></i>
+              <i class="fas fa-trash"></i>
+            </div>
           </v-list-item>
-              
+        </template>
       </v-virtual-scroll>
     </v-card>
-    </v-main>
-
+  </v-main>
 </template>
 
 <script>
 export default {
-    computed: {
-      items () {
-        return Array.from({ length: this.length })
-      },
-      length () {
-        return 5
-      },
+  props: {
+    products: Array,
+  },
+  computed: {
+    items() {
+      const proLength = this.products.length;
+
+      return Array.from({ length: proLength }, (k, v) => {
+        return {
+          _id: this.products[v]._id,
+          name: this.products[v].information.name,
+          price: this.products.price,
+        };
+      });
     },
-  }
+  },
+};
 </script>
 
-<style scoped lang= "scss">
+<style scoped lang="scss">
+.resumen {
+  width: 280px;
+  float: right;
 
-.main{
-    margin-top: 30px;
-}
-
-.resumen{
-    width: 280px;
-    float: right;
-    margin-right: 155px;
-
-    
-.titulo{
+  .titulo {
     text-transform: uppercase;
     font-family: $montserratSemiBold-font;
-    
-    .remi{
-    margin-top: 10px;
-    display: flex;
-    justify-content: space-around;
 
-    }
-
-    .descrip{
-    display: flex;
-    justify-content: space-around;
-    font-family: $montserratSemiBold-font;
-    font-size: 14px;
-    }
-
-    .divider{
-        background-color:#ff8585;
-        margin: 0px 25px 0 25px;
-    }
-
-}
-
-    .contenido{
-     display: flex;
-     justify-content: space-around;
-     font-size: 14px; 
-     font-family: $montserratRegular-font;
-     margin:7px 0px 7px 2px;
-    }
-    
-    .fin{
+    .remi {
+      margin-top: 10px;
       display: flex;
-      justify-content: space-between;
+      justify-content: space-around;
+    }
+
+    .descrip {
+      display: flex;
+      justify-content: space-around;
       font-family: $montserratSemiBold-font;
-      margin:50px 27px 7px 35px;
+      font-size: 14px;
     }
-    
-    .boton{
-      margin:7px 27px 10px 35px;
+
+    .divider {
+      background-color: #ff8585;
+      margin: 0px 25px 0 25px;
     }
+  }
+
+  .contenido {
+    display: flex;
+    justify-content: space-around;
+    font-size: 14px;
+    font-family: $montserratRegular-font;
+    margin: 7px 0px 7px 2px;
+  }
+
+  .fin {
+    display: flex;
+    justify-content: space-between;
+    font-family: $montserratSemiBold-font;
+    margin: 50px 27px 7px 35px;
+  }
+
+  .boton {
+    margin: 7px 27px 10px 35px;
+  }
 }
 
-.productos{
-   margin-left:155px;
-   font-family: $montserratSemiBold-font;
+.productos {
+  font-family: $montserratSemiBold-font;
 
-  .icons{
+  .icons {
     position: absolute;
     right: 20px;
   }
-  
-   i{
-     margin-left: 10px;
-   }
 
-    .item{
+  i {
+    margin-left: 10px;
+  }
+
+  .item {
     color: white;
-    }
+  }
 
-   .prendaInfo{
-       float: right;
-       margin-left: 20px;
-   }
-
-
+  .prendaInfo {
+    float: right;
+    margin-left: 20px;
+  }
 }
-
 </style>
--- client/src/views/MiCarrito.vue
+++ client/src/views/MiCarrito.vue
@@ -2,7 +2,7 @@
   <v-main>
     <Header />
     <SearchBar v-if="is_searchBar_open" />
-    <MiCarrito />
+    <MiCarrito class="width" :products="products" />
     <Footer />
   </v-main>
 </template>
@@ -20,6 +20,58 @@ export default {
     SearchBar,
     MiCarrito,
   },
+  data() {
+    return {
+      products: [
+        {
+          _id: "60653dd3cee1f513b44da5db",
+          information: {
+            name: "Camisa azul",
+            price: 157895,
+            short_description: "Camisa azul usada pocas veces por joven modelo",
+            description:
+              "Camisa azul usada pocas veces por joven modelo, Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+          },
+          tags: ["Azul", "Joven", "Estilo"],
+        },
+        {
+          _id: "60653ed532c62508a4d9964f",
+          information: {
+            name: "Pantalon azul",
+            price: 157895,
+            short_description:
+              "Pantalon azul usada pocas veces por joven modelo",
+            description:
+              "Camisa azul usada pocas veces por joven modelo, Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+          },
+          tags: ["Azul", "Joven", "Pantalon"],
+        },
+        {
+          _id: "60653f5332c62508a4d99653",
+          information: {
+            name: "Collar Verde",
+            price: 15855,
+            short_description:
+              "Collar verde usado pocas veces por joven modelo",
+            description:
+              "Camisa azul usada pocas veces por joven modelo, Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+          },
+          tags: ["Azul", "Joven", "Estilo"],
+        },
+        {
+          _id: "60653f6532c62508a4d99657",
+          information: {
+            name: "Camisa azul",
+            price: 157895,
+            short_description: "Camisa azul usada pocas veces por joven modelo",
+            description:
+              "Camisa azul usada pocas veces por joven modelo, Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+          },
+          tags: ["Azul", "Joven", "Estilo"],
+        },
+      ],
+    };
+  },
   computed: {
     is_searchBar_open: function() {
       return this.$store.getters.get_is_searchBar_open;
@@ -28,4 +80,10 @@ export default {
 };
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+.width {
+  width: 70vw;
+  margin: auto;
+  margin-top: 4.5rem;
+}
+</style>
