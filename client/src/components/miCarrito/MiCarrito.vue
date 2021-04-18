07a92aac8490e55cd9c38567993a1e57b624110d
Juliana <rojasnpaula@javeriana.edu.co>
2021-04-18 14:21:35 -0500

    carrito without script

--- client/src/components/miCarrito/MiCarrito.vue
+++ client/src/components/miCarrito/MiCarrito.vue
@@ -1,13 +1,160 @@
 <template>
-  <h1>Carrito</h1>
+    <v-main class="main">  
+
+        <v-card class="resumen">
+              <div class="titulo">
+               <p class="remi">Resumen de mi carrito</p>
+               <div class="descrip">
+                <p>Artículo</p>
+                <p>Precio</p>
+               </div>
+              <v-divider class="divider"></v-divider>
+              </div>
+
+              <div class="contenido"> <!--Aquí el for para el script-->
+                  <div class="prenda">Camisa azul</div>
+                  <div class="precio">$140,000</div>
+              </div>
+
+              <div class="fin">
+                 <div class="txtTotal">TOTAL</div>
+                 <div class="numTotal">$140,000</div> <!--Igual depende del script la suma-->
+              </div>
+
+              <v-btn class="boton"
+               rounded
+               color="#ff8585"
+               dark
+              >
+              Continuar con envío
+              </v-btn>
+        </v-card>
+
+    <v-card
+      elevation="0"
+      max-width="710"
+      class="productos"
+    >
+      <v-virtual-scroll
+        :items="items"
+        height="470"
+        item-height="200"
+      >
+          <v-list-item class="item">
+
+          <img src="../../assets/imgs/logo.png" height="100" width="100"/>
+  
+          <div class="prendaInfo">
+              <p>Camisa azul</p>
+              <p>$140,000 IVA incluido</p>
+          </div>
+
+          <div class="icons">
+           <i class="far fa-heart"></i>
+           <i class="fas fa-trash"></i>
+          </div>
+
+          </v-list-item>
+              
+      </v-virtual-scroll>
+    </v-card>
+    </v-main>
+
 </template>
 
 <script>
 export default {
-
-}
+    computed: {
+      items () {
+        return Array.from({ length: this.length })
+      },
+      length () {
+        return 5
+      },
+    },
+  }
 </script>
 
-<style>
+<style scoped lang= "scss">
+
+.main{
+    margin-top: 30px;
+}
+
+.resumen{
+    width: 280px;
+    float: right;
+    margin-right: 155px;
+
+    
+.titulo{
+    text-transform: uppercase;
+    font-family: $montserratSemiBold-font;
+    
+    .remi{
+    margin-top: 10px;
+    display: flex;
+    justify-content: space-around;
+
+    }
+
+    .descrip{
+    display: flex;
+    justify-content: space-around;
+    font-family: $montserratSemiBold-font;
+    font-size: 14px;
+    }
+
+    .divider{
+        background-color:#ff8585;
+        margin: 0px 25px 0 25px;
+    }
+
+}
+
+    .contenido{
+     display: flex;
+     justify-content: space-around;
+     font-size: 14px; 
+     font-family: $montserratRegular-font;
+     margin:7px 0px 7px 2px;
+    }
+    
+    .fin{
+      display: flex;
+      justify-content: space-between;
+      font-family: $montserratSemiBold-font;
+      margin:50px 27px 7px 35px;
+    }
+    
+    .boton{
+      margin:7px 27px 10px 35px;
+    }
+}
+
+.productos{
+   margin-left:155px;
+   font-family: $montserratSemiBold-font;
+
+  .icons{
+    position: absolute;
+    right: 20px;
+  }
+  
+   i{
+     margin-left: 10px;
+   }
+
+    .item{
+    color: white;
+    }
+
+   .prendaInfo{
+       float: right;
+       margin-left: 20px;
+   }
+
+
+}
 
 </style>
--- client/src/views/MiCarrito.vue
+++ client/src/views/MiCarrito.vue
@@ -2,6 +2,7 @@
   <v-main>
     <Header />
     <SearchBar v-if="is_searchBar_open" />
+    <MiCarrito />
     <Footer />
   </v-main>
 </template>
@@ -10,11 +11,14 @@
 import Header from "../components/general/Header.vue";
 import Footer from "../components/general/Footer";
 import SearchBar from "@/components/general/SearchBar";
+import MiCarrito from "@/components/miCarrito/MiCarrito";
+
 export default {
   components: {
     Header,
     Footer,
     SearchBar,
+    MiCarrito,
   },
   computed: {
     is_searchBar_open: function() {
