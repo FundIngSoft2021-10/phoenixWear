28873320b7fdf30785a9ad2763b5323bebe180b7
alejo2300 <fcamargodiaz@gmail.com>
2021-04-18 15:17:04 -0500

    Purchase confirmation added!

---
+++ client/src/components/Purchase/purchaseConfirmation.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="confirmationPurchase">
+    <div class="title">
+      <h1>¡PASO REALIZADO!</h1>
+    </div>
+    <div class="image">
+      <img src="../../assets/imgs/succesfulPurchase.png"/>
+    </div>
+  </div>
+
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped lang="scss">
+
+.title{
+  text-align: center;
+  padding-top: 1.5rem;
+  font-family: $montserratRegular-font;
+}
+
+.image{
+  margin: auto;
+  width: 65%;
+}
+</style>
---
+++ client/src/views/PurchaseSuccess.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-main>
+    <Header />
+    <SearchBar v-if="is_searchBar_open" />
+    <p-confirmation/>
+    <Footer />
+    <!-- <img src="../assets/imgs/logo.png" alt="Phoenix-logo" height="200" width="200" /> -->
+  </v-main>
+</template>
+
+<script>
+import Header from "../components/general/Header.vue";
+import pConfirmation from "../components/Purchase/purchaseConfirmation.vue";
+import Footer from "../components/general/Footer";
+import SearchBar from "@/components/general/SearchBar";
+export default {
+  components: {
+    Header,
+    Footer,
+    pConfirmation,
+    SearchBar,
+  },
+  computed: {
+    is_searchBar_open: function() {
+      return this.$store.getters.get_is_searchBar_open;
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.sad {
+  height: 100vh;
+}
+</style>
