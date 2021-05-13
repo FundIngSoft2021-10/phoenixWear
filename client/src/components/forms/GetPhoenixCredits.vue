8ccf088cc98c4d1cb2b37133a76fc7acec6a7c9c
Juan Carlos Suarez <suarezmjuan@gmail.com>
2021-05-13 09:13:26 -0500

    Creation of credits page

---
+++ client/src/components/forms/GetPhoenixCredits.vue
@@ -0,0 +1,53 @@
+<template>
+    <v-form ref="form" v-model="valid" lazy-validation>
+        <v-container class="credits">
+            <form method="POST" action="https://formspree.io/f/mbjqjand">
+                    <v-text-field
+                        name="Username"
+                        :counter="20"
+                        label="Username"
+                        required
+                        :rules="text_rule"
+                    />
+                    <v-text-field
+                        name="Cantidad"
+                        label="Cantidad"
+                        required
+                    />
+                    <v-btn
+                        type="submit"
+                        name="send"
+                        class="send"
+                        color="phoenix"
+                        dark
+                        large
+                        rounded
+                        >
+                        Agregar Créditos
+                    </v-btn>
+            </form>           
+        </v-container>
+    </v-form>
+</template>
+<script></script>
+<style scoped lang="scss">
+.width {
+  width: 70vw;
+  margin: auto;
+  font-family: $montserratRegular-font;
+  @media screen and (max-width: $breakpoint-desktop) {
+    width: 90vw;
+  }
+  @media screen and (max-width: $breakpoint-tablet) {
+    width: 100vw;
+    margin: 0;
+  }
+}
+.credits{
+    font-family: $montserratRegular-font;
+}
+.credits #cantidad{
+    background-color: red;
+}
+    
+</style>
---
+++ client/src/views/GetPhoenixCredits.vue
@@ -0,0 +1,25 @@
+<template>
+    <v-main>
+        <Header/>
+        <Credits class="width"/>
+        <Footer/>        
+    </v-main>
+</template>
+<script>
+import Header from "../components/general/Header.vue";
+import Footer from "../components/general/Footer";
+import Credits from "../components/forms/GetPhoenixCredits"
+export default {
+  components: {
+    Header,
+    Footer,
+    Credits,
+  },
+  computed: {
+    is_searchBar_open: function() {
+      return this.$store.getters.get_is_searchBar_open;
+    },
+  },
+};
+</script>
+<style lang="stylus" scoped></style>
