570787cbdb377d35df68a163c53b11b9a1b5e4d1
Juan Carlos Suarez <suarezmjuan@gmail.com>
2021-05-21 07:51:02 -0500

    Basic structure to include products

--- server/app.js
+++ server/app.js
@@ -6,7 +6,7 @@ const recordsUser = require("./routes/users");
 const recordsProduct = require("./routes/products");
 const recordsTransaction = require("./routes/transactions");
 const swaggerUi = require("swagger-ui-express");
-const users = require("./users.json");
+const swagger = require("./swagger.json");
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -16,6 +16,6 @@ app.use("/records", recordsRouter);
 app.use("/users", recordsUser);
 app.use("/products", recordsProduct);
 app.use("/transactions", recordsTransaction);
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(users));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swagger));
 
 module.exports = app;
